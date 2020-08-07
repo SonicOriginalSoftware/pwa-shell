4012b562bb5e61d46a5d046c7d8e1a1b0ecf099c
Nathan Blair <engineer.nblair@gmail.com>
2020-08-06 22:34:55 -0400

    added service worker library functions for app shell

---
+++ app/lib/service-worker/service_worker.js
@@ -0,0 +1,32 @@
+export const messages = Object.freeze({
+  get_app_info: "get_app_info",
+  get_db_info: "get_db_info",
+  get_oauth_info: "get_oauth_info",
+  update: "update"
+})
+
+/**
+ * @param {keyof messages} message
+ * @param {ServiceWorker | null} sw
+ *
+ * @returns {Promise<{name: String, version: String}>}
+ */
+export async function message(
+  message,
+  sw = navigator.serviceWorker.controller
+) {
+  if (!sw) {
+    return Promise.reject("Service worker not ready for messaging")
+  }
+  const messageChannel = new MessageChannel()
+  messageChannel.port1.start() // Required when using eventListener interface
+
+  const messagePromise = new Promise((resolve, reject) => {
+    messageChannel.port1.addEventListener("messageerror", (ev) => reject(ev))
+    messageChannel.port1.addEventListener("message", (ev) => resolve(ev.data))
+  })
+
+  sw.postMessage(message, [messageChannel.port2])
+
+  return messagePromise
+}
